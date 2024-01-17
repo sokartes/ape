8d94cb62247499ad842f9b10ca28c326da3cfdeb
sokartes <156802757+sokartes@users.noreply.github.com>
2024-01-17 05:20:20 +0300

    Add doc comments

--- animation.go
+++ animation.go
@@ -2,16 +2,19 @@ package ape
 
 import "github.com/hajimehoshi/ebiten/v2"
 
+// Animation for AnimationPlayer
 type Animation struct {
 	Frames []*ebiten.Image
 	FPS    float64
 	Name   string
 }
 
+// FrameCount returns the frame count of the animation
 func (a *Animation) FrameCount() int {
 	return len(a.Frames)
 }
 
+// NewAnimation returns new Animation
 func NewAnimation(frames []*ebiten.Image, name string) *Animation {
 	return &Animation{
 		Frames: frames,
--- animplayer.go
+++ animplayer.go
@@ -7,6 +7,7 @@ import (
 	"github.com/hajimehoshi/ebiten/v2"
 )
 
+// AnimationPlayer plays and manages animations.
 type AnimationPlayer struct {
 	SpriteSheet, CurrentFrame *ebiten.Image
 	Animations                map[string]*Animation
@@ -17,6 +18,13 @@ type AnimationPlayer struct {
 	tick         float64
 }
 
+// NewAnimation adds new Animation to this AnimationPlayer and returns the added animation.
+//
+// x and y are top-left coordinates of the first frame's rectangle.
+//
+// w and h are the width and height of the first frame's rectangle.
+//
+// The sub-rectangles repeat to the right by the frameCount amount.
 func (ap *AnimationPlayer) NewAnimation(stateName string, x, y, w, h, frameCount int) *Animation {
 	subImages := []*ebiten.Image{}
 	frameRect := image.Rect(x, y, x+w, y+h)
@@ -35,24 +43,30 @@ func (ap *AnimationPlayer) NewAnimation(stateName string, x, y, w, h, frameCount
 	return anim
 }
 
+// SetFPS overwrites the FPS of all animations.
 func (ap *AnimationPlayer) SetFPS(fps float64) {
 	for _, anim := range ap.Animations {
 		anim.FPS = fps
 	}
 }
 
+// AddAnimation adds the given animation to this player.
+// Adds the name of the animation as a map key.
 func (ap *AnimationPlayer) AddAnimation(a *Animation) {
 	ap.Animations[a.Name] = a
 }
 
+// State returns current active animation state
 func (ap *AnimationPlayer) State() string {
 	return ap.currentState
 }
 
+// CurrentStateFPS returns FPS of the current animation state
 func (ap *AnimationPlayer) CurrentStateFPS() float64 {
 	return ap.Animations[ap.State()].FPS
 }
 
+// SetState sets the current animation state. Each time the state changes, the animation resets to the first frame.
 func (ap *AnimationPlayer) SetState(state string) {
 	if ap.currentState != state {
 		ap.currentState = state
@@ -61,11 +75,15 @@ func (ap *AnimationPlayer) SetState(state string) {
 	}
 }
 
+// PauseAtFrame pauses the current animation at the frame. If index is out of range it does nothing.
 func (ap *AnimationPlayer) PauseAtFrame(frameIndex int) {
-	ap.Paused = true
-	ap.CurrentFrameIndex = frameIndex
+	if frameIndex < ap.Animations[ap.State()].FrameCount() && frameIndex >= 0 {
+		ap.Paused = true
+		ap.CurrentFrameIndex = frameIndex
+	}
 }
 
+// Update updates AnimationPlayer
 func (ap *AnimationPlayer) Update() {
 	if !ap.Paused {
 		ap.tick += ap.Animations[ap.currentState].FPS / 60.0
@@ -80,6 +98,7 @@ func (ap *AnimationPlayer) Update() {
 	ap.CurrentFrame = ap.Animations[ap.currentState].Frames[ap.CurrentFrameIndex]
 }
 
+// NewAnimationPlayer returns new AnimationPlayer with spriteSheet
 func NewAnimationPlayer(spriteSheet *ebiten.Image) *AnimationPlayer {
 	return &AnimationPlayer{
 		SpriteSheet:       spriteSheet,
